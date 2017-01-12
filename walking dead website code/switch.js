80ce611a2e3ed8d7cace56b9368b4408fe9bae2f
LouisSimpson <louissimpson1996@gmail.com>
2017-01-12 22:47:35 +0000

    Initial upload

---
+++ walking dead website code/switch.js
@@ -0,0 +1,11 @@
+function switch_style() 
+				{
+					document.getElementById('stylesheet1').href='cssindex2.css';
+				}
+			
+
+function switch_back() 
+				{
+					document.getElementById('stylesheet1').href='cssindex.css';
+				}
+			
